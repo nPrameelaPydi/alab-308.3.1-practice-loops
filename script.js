9c1c62d09c63025f6cba55e7b80d5a3b5e673cfd
Prameela Pydi <nprameela.pydi@gmail.com>
2024-09-15 22:46:40 -0700

    un-commented the part-1, part-2 code

--- script.js
+++ script.js
@@ -7,18 +7,18 @@ console.log("practice loops");
 //If a number is divisible by both 3 and 5, log “Fizz Buzz.”
 //If a number is not divisible by either 3 or 5, log the number.
 
-//console.log(`*****Part 1*********`);
-//for (let i = 1; i <= 100; i++) {
-//    if (i % 3 == 0 && i % 5 == 0) {
-//        console.log("Fizz Buzz");
-//    } else if (i % 3 == 0) {
-//        console.log("Fizz");
-//    } else if (i % 5 == 0) {
-//        console.log("Buzz");
-//    } else {
-//        console.log(i);
-//    }
-//}
+console.log(`*****Part 1*********`);
+for (let i = 1; i <= 100; i++) {
+    if (i % 3 == 0 && i % 5 == 0) {
+        console.log("Fizz Buzz");
+    } else if (i % 3 == 0) {
+        console.log("Fizz");
+    } else if (i % 5 == 0) {
+        console.log("Buzz");
+    } else {
+        console.log(i);
+    }
+}
 
 
 //(`*****Part 2*********`);
@@ -26,29 +26,29 @@ console.log("practice loops");
 //Create a loop that searches for the next prime number, starting at n and incrementing from there.
 //As soon as you find the prime number, log that number and exit the loop.
 
-//console.log(`*****************************************************`);
-//console.log(`*****Part 2*********`);
-//let n = 120;
-////let isPrime = true;
-//for (let i = n + 1; ; i++) {
-//    let isPrime = true;
+console.log(`                                          `);
+console.log(`*****Part 2*********`);
+let n = 120;
+//let isPrime = true;
+for (let i = n + 1; ; i++) {
+    let isPrime = true;
 
-//    for (let j = 2; j <= i / 2; j++) {
+    for (let j = 2; j <= i / 2; j++) {
 
-//        if (i % j == 0) {
-//            isPrime = false;
-//        }
-//    }
+        if (i % j == 0) {
+            isPrime = false;
+        }
+    }
 
-//    if (isPrime) {
-//        console.log(i);
-//        break;
-//    }
-//}
+    if (isPrime) {
+        console.log(i);
+        break;
+    }
+}
 
 
-//console.log(`*****************************************************`);
-//console.log(`*****Part 3*********`);
+console.log(`                                          `);
+console.log(`*****Part 3*********`);
 
 let csv = "Index,Mass (kg),Spring 1 (m),Spring 2 (m)\n1,0.0.050,0.050\n2,0.49,0.066,0.066\n3,0.98,0.0800,7,0.080\n4,1.47,0.116,0.108\n5,1.96,0.142,0.138\n6,2.45,0.166,0.158\n7,2.94,0.193,0.174\n8,3.43,0.204,0.192\n9,3.92,0.226,0.205\n10,4.41,0.238,0.232\n";
 
