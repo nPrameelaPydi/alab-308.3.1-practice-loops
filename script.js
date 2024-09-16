7ed21554331a8ed552b322ba362db5b6abc10e14
Prameela Pydi <nprameela.pydi@gmail.com>
2024-09-15 17:37:36 -0700

    added part-2

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 console.log("practice loops");
 
-console.log(`*****Part 1*********`);
+//console.log(`*****Part 1*********`);
 //Loop through all numbers from 1 to 100.
 //If a number is divisible by 3, log “Fizz.”
 //If a number is divisible by 5, log “Buzz.”
 //If a number is divisible by both 3 and 5, log “Fizz Buzz.”
 //If a number is not divisible by either 3 or 5, log the number.
 
+console.log(`*****Part 1*********`);
 for (let i = 1; i <= 100; i++) {
     if (i % 3 == 0 && i % 5 == 0) {
         console.log("Fizz Buzz");
@@ -18,5 +19,33 @@ for (let i = 1; i <= 100; i++) {
         console.log(i);
     }
 }
+
+
+//(`*****Part 2*********`);
+//Declare an arbitrary number, n.
+//Create a loop that searches for the next prime number, starting at n and incrementing from there.
+//As soon as you find the prime number, log that number and exit the loop.
+
+//console.log(`*****************************************************`);
+console.log(`*****Part 2*********`);
+let n = 120;
+//let isPrime = true;
+for (let i = n + 1; ; i++) {
+    let isPrime = true;
+
+    for (let j = 2; j <= i / 2; j++) {
+
+        if (i % j == 0) {
+            isPrime = false;
+        }
+    }
+
+    if (isPrime) {
+        console.log(i);
+        break;
+    }
+}
+
+
 
 
